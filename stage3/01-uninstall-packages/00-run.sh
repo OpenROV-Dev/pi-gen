9d0b492c24b8301b520233009f0e838db432950d
Charles Cross <charles@openrov.com>
2016-05-04 13:57:22 -0700

    Fixes and updates to the openrov customization script

--- stage3/01-uninstall-packages/00-run.sh
+++ stage3/01-uninstall-packages/00-run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
 # Update and package list and remove unnecessary packages
-on_chroot sh -e - <<OROV_EOF
-# Remove following
-apt-get purge \
-ed \
-lua5.1 \
-luajit \
-python* \
-rsyslog \
-logrotate \
-man-db \
-manpages \
-manpages-dev -y
+# on_chroot sh -e - <<OROV_EOF
+# # Remove following
+# apt-get purge \
+# ed \
+# lua5.1 \
+# luajit \
+# python* \
+# rsyslog \
+# logrotate \
+# man-db \
+# manpages \
+# manpages-dev -y
 
-# Purge
-apt-get autoremove --purge -y
-OROV_EOF
+# # Purge
+# apt-get autoremove --purge -y
+# OROV_EOF

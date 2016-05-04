b02571c02388ef7a21e56b6930cf41840878f7ca
Charles Cross <charles@openrov.com>
2016-05-03 20:57:33 -0700

    Added permission changing script, fixed some things in openrov stages

--- stage3/02-install-packages/01-run.sh
+++ stage3/02-install-packages/01-run.sh
@@ -1,20 +1,20 @@
 
 # Install cloud9 using Robert Nelson's repos
 # TODO: Build this ourselves?
-on_chroot sh -e - <<EOF
-	# Backup sources list
-	cp /etc/apt/sources.list /etc/apt/sources.list.bk
+# on_chroot sh -e - <<EOF
+# 	# Backup sources list
+# 	cp /etc/apt/sources.list /etc/apt/sources.list.bk
 
-	# Add RCN repos
-	echo "deb http://repos.rcn-ee.com/debian/ jessie main" | tee --append /etc/apt/sources.list
+# 	# Add RCN repos
+# 	echo "deb http://repos.rcn-ee.com/debian/ jessie main" | tee --append /etc/apt/sources.list
 
-	apt-get update
+# 	apt-get update
 
-	# RCN Packages
-	apt-get install c9-core-installer
+# 	# RCN Packages
+# 	apt-get install c9-core-installer -y --force-yes
 
-	# Restore apt source list
-	mv /etc/apt/sources.list.bk /etc/apt/source.list
+# 	# Restore apt source list
+# 	mv /etc/apt/sources.list.bk /etc/apt/source.list
 	
-	apt-get update
-EOF
+# 	apt-get update
+# EOF
--- stage3/03-install-openrov-packages/00-run.sh
+++ stage3/03-install-openrov-packages/00-run.sh
@@ -4,4 +4,6 @@
 on_chroot sh -e - <<EOF
 	echo "deb http://deb-repo.openrov.com jessie unstable" | tee --append /etc/apt/sources.list
 	wget -O - -q http://deb-repo.openrov.com/build.openrov.com.gpg.key | apt-key add -
+	
+	apt-get update
 EOF

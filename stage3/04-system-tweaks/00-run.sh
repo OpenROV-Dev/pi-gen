d85e284158e6fd2b77e83fc2e59da45874a1e5b9
Charles Cross <charles@openrov.com>
2016-05-06 03:22:04 -0700

    Disabled hciuart service

--- stage3/04-system-tweaks/00-run.sh
+++ stage3/04-system-tweaks/00-run.sh
@@ -7,6 +7,11 @@ echo "vm.dirty_ratio = 10" >> ${ROOTFS_DIR}/etc/sysctl.conf
 # NOTE: May not be necessary because of 'fsck.repair=yes' in boot cmdline.txt
 sed -i 's/FSCKFIX=no/FSCKFIX=yes/g' ${ROOTFS_DIR}/lib/init/vars.sh
 
+# Disable hciuart service, since we disabled bluetooth
+on_chroot sh -e - <<EOF
+systemctl disable hciuart
+EOF
+
 # Add admin group and put the admin user in it
 on_chroot sh -e - <<EOF
 groupadd -f -r admin

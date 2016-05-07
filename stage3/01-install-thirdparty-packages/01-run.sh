5220df2571c6a940804ff9cfbed9b76d72f51e81
Charles Cross <charles@openrov.com>
2016-05-06 18:14:54 -0700

    Added install of rpi-update and call it to upgrade the kernel

--- stage3/01-install-thirdparty-packages/01-run.sh
+++ stage3/01-install-thirdparty-packages/01-run.sh
@@ -1,20 +1,6 @@
+#!/bin/bash -e
 
-# Install cloud9 using Robert Nelson's repos
-# TODO: Build this ourselves?
-# on_chroot sh -e - <<EOF
-# 	# Backup sources list
-# 	cp /etc/apt/sources.list /etc/apt/sources.list.bk
-
-# 	# Add RCN repos
-# 	echo "deb http://repos.rcn-ee.com/debian/ jessie main" | tee --append /etc/apt/sources.list
-
-# 	apt-get update
-
-# 	# RCN Packages
-# 	apt-get install c9-core-installer -y --force-yes
-
-# 	# Restore apt source list
-# 	mv /etc/apt/sources.list.bk /etc/apt/source.list
-	
-# 	apt-get update
-# EOF
+# Run rpi-update to get latest kernel
+on_chroot sh -e - <<EOF
+SKIP_WARNING=1 rpi-update
+EOF
